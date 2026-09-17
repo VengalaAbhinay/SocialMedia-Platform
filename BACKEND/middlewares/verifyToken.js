698e74688d0e7c7bfb67383fd7528565f0fafc9b
VengalaAbhinay <vengalaabhinay2006@gmail.com>
2026-09-17 09:50:36 +0530

    Fix model import paths for deployment

--- BACKEND/middlewares/verifyToken.js
+++ BACKEND/middlewares/verifyToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { UserModel } from '../Models/UserModel.js'
+import { UserModel } from '../models/UserModel.js'
 
 export const verifyToken = async (req, res, next) => {
   const token = req.cookies.token
